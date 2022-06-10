b995b2fcd2820d00c2040fe45c2259e95cfc0dae
Kade <kadeesterline@gmail.com>
2022-06-10 11:39:34 -0700

    adds navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div id="navbar" className="bg-slate-300 h-9 w-100">
+      <nav className="flex justify-end">
+        <NavLink className="px-2 py-1/2" to="/">
+          Home
+        </NavLink>
+        <NavLink className="px-2 py-1/2" to="/gallery">
+          Gallery
+        </NavLink>
+        <NavLink className="px-2 py-1/2" to="/contact">
+          Contact
+        </NavLink>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
