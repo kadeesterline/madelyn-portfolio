b1168acbbb093e424457aa8626bc19cd55be9ffc
Kade <kadeesterline@gmail.com>
2022-06-10 11:30:47 -0700

    adds react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import Home from "./Pages/Home";
+import Gallery from "./Pages/Gallery";
+import Contact from "./Pages/Contact";
+
+function NavLayout() {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route element={<NavLayout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
