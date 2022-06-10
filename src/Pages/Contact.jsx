fde32cb44cf6ffda8d3e16293b488a6cd58da658
Kade <kadeesterline@gmail.com>
2022-06-10 13:25:31 -0700

    styles contact form

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,7 +3,10 @@ import ContactForm from "../Components/ContactForm";
 
 function Contact() {
   return (
-    <div>
+    <div className="grid justify-center">
+      <div className="flex justify-center">
+        <h1 className="text-4xl pt-5 m-2">Contact Me</h1>
+      </div>
       <ContactForm />
     </div>
   );
