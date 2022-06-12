45cef9ce76e9b87edccfb3bc0f6a01bac8ccc3d2
Kade <kadeesterline@gmail.com>
2022-06-11 17:51:57 -0700

    adds more pictures

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import hero from "../Images/IMG_8153.PNG";
+import horseshoe from "../Images/IMG_8153.PNG";
+import bay from "../Images/IMG_8181.PNG";
+import foxglove from "../Images/IMG_8182.PNG";
 import { FaInstagram, FaHeart } from "react-icons/fa";
 
 function Home() {
@@ -16,19 +18,29 @@ function Home() {
           <FaInstagram />
         </a>
       </div>
-      <div className="flex justify-center border-t-2 border-slate-400">
-        <p className="w-96 p-2 m-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est inventore
-          voluptate, repudiandae alias impedit rem quas atque optio asperiores
-          quaerat nam, aspernatur ex magni ullam debitis provident? Harum,
-          aspernatur tenetur.
-        </p>
-      </div>
-      {/* <img src={hero} alt="horseshoe bend" className="m-3" /> */}
       <div className="flex justify-center">
-        <p className="w-100">
-          <a href="https://kadecodes.dev">Made by Kade Esterline</a>
-        </p>
+        <div className="flex justify-center w-96 border-t-2 border-slate-400">
+          <p className="w-100 p-2 m-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+            inventore voluptate, repudiandae alias impedit rem quas atque optio
+            asperiores quaerat nam, aspernatur ex magni ullam debitis provident?
+            Harum, aspernatur tenetur.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <img src={horseshoe} alt="horseshoe bend" className="m-3 w-96" />
+        <img src={bay} alt="bay" className="m-3 w-96" />
+        <img src={foxglove} alt="foxglove" className="m-3 w-96" />
+      </div>
+
+      <div className="flex justify-center">
+        <div className="flex justify-center w-96 border-t-2 border-slate-400">
+          <p className="w-100 m-2 p-2">
+            <a href="https://kadecodes.dev">Made by Kade Esterline</a>
+          </p>
+        </div>
       </div>
     </div>
   );
